21000447a0ef510514d992d59a8e512f422dc506
Andrew GALAVAN <agalavan@student.unit.ua>
2018-09-25 16:24:43 +0300

    add account-settings view, not finished

--- app/Controllers/Auth/PasswordController.php
+++ app/Controllers/Auth/PasswordController.php
@@ -7,13 +7,17 @@ use Matcha\Models\User;
  * use покажет какой родительский контроллер нужно использовать
  * */
 use Matcha\Controllers\Controller;
+use Matcha\Controllers\Check\CheckController;
 use Respect\Validation\Validator as v;
 
 class PasswordController extends Controller
 {
     public function getChangePassword($request, $response)
     {
-        return $this->view->render($response, 'user/edit/password.twig');
+        $userInfo = $this->checker->user();
+        $this->container->view->getEnvironment()->addGlobal('email', $userInfo->email);
+
+        return $this->view->render($response, 'user/edit/account-settings.twig');
     }
 
     public function postChangePassword($request, $response)
@@ -31,6 +35,7 @@ class PasswordController extends Controller
              * 
              * с чем он сравнивает? c объектом v:: ?
              * */
+           'email' => v::noWhitespace()->notEmpty()->email()->emailAvailable(),
            'password_old' => v::noWhitespace()->notEmpty()->matchesPassword($this->checker->user()->password),
            'password_new' => v::noWhitespace()->notEmpty(),
            'password_repeat' => v::noWhitespace()->notEmpty(),
@@ -40,6 +45,8 @@ class PasswordController extends Controller
             return $response->withRedirect($this->router->pathFor('auth.password.change'));
         }
 
+        $email = $request->getParam('email');
+        $this->checker->user()->setEmail($_SESSION['user'], $email);
         $password1 = $request->getParam('password_new');
         $password2 = $request->getParam('password_repeat');
 
@@ -48,7 +55,7 @@ class PasswordController extends Controller
 
         $this->checker->user()->setPassword($request->getParam('password_new'));
 
-        $this->flash->addMessage('info', 'Your password was changed');
+        // $this->flash->addMessage('info', 'Your password was changed');
         return $response->withRedirect($this->router->pathFor('home'));
     }
 }
