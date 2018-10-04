b76eca65f345a30f5ea276fc08abf1fadc7a162c
Andrew GALAVAN <agalavan@student.unit.ua>
2018-10-04 14:48:20 +0300

    set-up account-settings page

--- app/Controllers/Auth/PasswordController.php
+++ app/Controllers/Auth/PasswordController.php
@@ -3,64 +3,86 @@
 namespace Matcha\Controllers\Auth;
 
 use Matcha\Models\User;
-/*
- * use покажет какой родительский контроллер нужно использовать
- * */
 use Matcha\Controllers\Controller;
 use Matcha\Controllers\Check\CheckController;
 use Respect\Validation\Validator as v;
 
 class PasswordController extends Controller
 {
-    public function getChangePassword($request, $response)
-    {
-        $userInfo = $this->checker->user();
-        $this->container->view->getEnvironment()->addGlobal('email', $userInfo->email);
+	public function getChangePassword($request, $response)
+	{
+		$userInfo = $this->checker->user();
+		$this->container->view->getEnvironment()->addGlobal('email', $userInfo->email);
 
-        return $this->view->render($response, 'user/edit/account-settings.twig');
-    }
+		return $this->view->render($response, 'user/edit/account-settings.twig');
+	}
 
-    public function postChangePassword($request, $response)
-    {
+	/**
+	*	Below we have multy step validation and error handling to update
+	*	user information.
+	*	Please read comments to understand every step
+	*/
+	public function postChangePassword($request, $response)
+	{
+		/*
+		** First validate email.
+		** - If it is valid and avialable in system.
+		** - If yes and user really create new one, update email in DB 
+		** and add a flash message
+		*/
+		$validation = $this->validator->validate($request, [
+			'email' => v::noWhitespace()->notEmpty()->email()->emailAvailable(),
+		]);
+		if ($validation->failed()) {
+			return $response->withRedirect($this->router->pathFor('auth.password.change'));
+		}
 
-        $validation = $this->validator->validate($request, [
-            /*
-             * password задесь возвращается как свойство
-             * которое храниться в объекте User
-             * все остальное как метод
-             * $this->auth->user()->password
-             * 
-             * вызовиться метод user() что вернет все что имется в БД
-             * из всего этого выбереться только свойство password
-             * 
-             * с чем он сравнивает? c объектом v:: ?
-             * */
-           'email' => v::noWhitespace()->notEmpty()->email()->emailAvailable(),
-           'password_old' => v::noWhitespace()->notEmpty()->matchesPassword($this->checker->user()->password),
-           'password_new' => v::noWhitespace()->notEmpty(),
-           'password_repeat' => v::noWhitespace()->notEmpty(),
-        ]);
+		$email = $request->getParam('email');
+		if ($email !== User::find($_SESSION['user'])->email) {
+			$this->checker->user()->setEmail($_SESSION['user'], $email);
+			$this->flash->addMessage('info', 'Your email was updated');
+		}
 
-        if ($validation->failed()) {
-            return $response->withRedirect($this->router->pathFor('auth.password.change'));
-        }
+		/*
+		** Next check password.
+		** - Check if fields are not empty.
+		** - If old password is correct.
+		** - If new password secure enough
+		** - Compare password_new and password_repeat
+		** 
+		** After all validation update password and add success message
+		*/
+		$validation = $this->validator->validate($request, [
+			'password_old' => v::noWhitespace()->notEmpty(),
+			'password_new' => v::noWhitespace()->notEmpty(),
+			'password_repeat' => v::noWhitespace()->notEmpty(),
+		]);
+		if ($validation->failed()) {
+			return $response->withRedirect($this->router->pathFor('auth.password.change'));
+		}
 
-        $email = $request->getParam('email');
-        $this->checker->user()->setEmail($_SESSION['user'], $email);
-        $password1 = $request->getParam('password_new');
-        $password2 = $request->getParam('password_repeat');
+		$validation = $this->validator->validate($request, [
+			'password_old' => v::matchesPassword($this->checker->user()->password),
+		]);
+		if ($validation->failed()) {
+			$this->flash->addMessage('error', 'Old password is incorrect, click "I forgot my password" to reset');
+			return $response->withRedirect($this->router->pathFor('auth.password.change'));
+		}
 
-        if (!preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/",$password1)) {
-            $this->flash->addMessage('error', 'Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit');
-            return $response->withRedirect($this->router->pathFor('auth.password.change'));
-        }
-        if ($this->checker->comparePasswords($password1, $password2, $response)) {
-            return $response->withRedirect($this->router->pathFor('auth.password.change'));
-        }
+		$password1 = $request->getParam('password_new');
+		$password2 = $request->getParam('password_repeat');
+		if (!preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $password1)) {
+			$this->flash->addMessage('error', 'Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit');
+			return $response->withRedirect($this->router->pathFor('auth.password.change'));
+		}
+		if ($this->checker->comparePasswords($password1, $password2, $response)) {
+			$this->flash->addMessage('error', 'New passwords does not match');
+			return $response->withRedirect($this->router->pathFor('auth.password.change'));
+		}
 
-        $this->checker->user()->setPassword($request->getParam('password_new'));
+		$this->checker->user()->setPassword($request->getParam('password_new'));
 
-        // $this->flash->addMessage('info', 'Your password was changed');
-        return $response->withRedirect($this->router->pathFor('home'));
-    }
+		$this->flash->addMessage('info', 'Your account was updated');
+		return $response->withRedirect($this->router->pathFor('auth.password.change'));
+	}
 }
